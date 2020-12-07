054a5ee04298e0e3bde93dfa22372961d0950fd4
chenpeng <715672907@qq.com>
2020-12-07 19:05:32 +0800

    自动生成测试文件夹

--- src/unitTest.java
+++ src/unitTest.java
@@ -5,10 +5,14 @@ public class unitTest {
         File file = new File(filename);
         try{
             if(file.isDirectory()){
-                System.out.println(new Tree(file.getAbsolutePath()));
+                Tree tree = new Tree(file.getAbsolutePath());
+                System.out.println(tree);
+                assert tree.getName().length() == 40;
             }
             else{
-                System.out.println(new Blob(file.getAbsolutePath()));
+                Blob blob = new Blob(file.getAbsolutePath());
+                System.out.println(blob);
+                assert blob.getName().length() == 40;
             }
         }
         catch (Exception ex){
@@ -31,10 +35,50 @@ public class unitTest {
         }
     }
 
+    public static void testName(File file){
+        try{
+            if(file.isDirectory()){
+                Tree tree = new Tree(file.getAbsolutePath());
+                System.out.println(tree);
+                assert tree.getName().length() == 40;
+            }
+            else{
+                Blob blob = new Blob(file.getAbsolutePath());
+                System.out.println(blob);
+                assert blob.getName().length() == 40;
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void testContent(File file){
+        try{
+            if(file.isDirectory()){
+                System.out.println(new Tree(file.getAbsolutePath()).getContent());
+            }
+            else{
+                System.out.println(new Blob(file.getAbsolutePath()));
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static File genFile(){
+        File root = new File("root");
+        File blob1 = new File(root,"blob1");
+        File tree1 = new File(root,"tree1");
+        File blob2 = new File(tree1,"blob2");
+        return root;
+    }
+
     public static void main(String[] args){
-        String filename = ".";
-        testName(filename);
-        testContent(filename);
+        File root = unitTest.genFile();
+        testName(root);
+        testContent(root);
 
     }
 }
